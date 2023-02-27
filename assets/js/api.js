0292b61f2c86da5aab910545f3159382b4d48aac
Richard Ribeiro <richard.diass@hotmail.com>
2023-02-26 21:15:44 -0300

    Atualizando o namo do repositorio

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/richardpaim/curriculo-Online-JS/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/richardpaim/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
